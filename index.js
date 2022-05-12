b0bdd54afc19b8ad67ac35921218805d2590d220
marios <mgeorgak859@gmail.com>
2022-05-12 16:00:29 +0300

    first design

---
+++ index.js
@@ -0,0 +1,15 @@
+const numberOfCells = 76
+
+function displayCells() {
+    let cellsContainer = document.getElementById('cells-container')
+    for (let cellCount = 1 ; cellCount<=numberOfCells ; cellCount++)
+    {
+        let cellNode = document.createElement('div')
+        let cellText = document.createElement('span')
+        cellNode.appendChild(cellText)
+       cellText.innerText = cellCount
+        cellNode.classList.add('cell')
+        cellsContainer.appendChild(cellNode)
+    }
+}
+displayCells();
